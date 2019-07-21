fb882c07b851adea995a934bc838149c9c6e2bc8
Quan Nguyen <quan.nguyen@midasprotocol.io>
2019-07-21 07:36:47 +0700

    cors

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", req.headers.origin);
   }
 
-  res.setHeader("Access-Control-Allow-Origin", " ");
+  res.setHeader("Access-Control-Allow-Origin", "*");
 
  
   res.setHeader(
